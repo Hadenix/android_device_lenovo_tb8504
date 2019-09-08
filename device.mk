cffbf124f595df9fa3dd962181c3fa2147e40a31
Erfan Abdi <erfangplus@gmail.com>
2019-09-08 15:04:55 +0430

    tb8504: ims: make shim for libui
    P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
    Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel = nullptr, int32_t* outBytesPerStride = nullptr);
    Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv"
    
    Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- device.mk
+++ device.mk
@@ -209,7 +209,8 @@ PRODUCT_PACKAGES += \
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
-    ims_ext_common.xml
+    ims_ext_common.xml \
+    libui_shim
 
 # IPA Manager
 PRODUCT_PACKAGES += \
